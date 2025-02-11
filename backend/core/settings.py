0e3709d696ac94d72af0c75fa61ede2400980dc8
Daniel Josh Uduma <116913814+Danieljosh-uduma@users.noreply.github.com>
2025-02-11 15:45:32 +0000

    migrating to postgresql

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -79,9 +79,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'patcoinDb',
+        'USER': 'postgres',
+        'PASSWORD': 'j0shn!3l',
+        'HOST': '0.0.0.0',
+        'PORT': '',
     }
 }
 
